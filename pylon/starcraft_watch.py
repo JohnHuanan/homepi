edbafb85ea127b522a740b25f05f3323942e5b77
MateMatiker <fabian.utesch@scienceforfun.org>
2019-12-03 22:10:00 +0100

    added pylon code

---
+++ pylon/starcraft_watch.py
@@ -0,0 +1,22 @@
+
+# This
+
+# set pylon to off
+
+# while  (true)
+
+    # if time = 06:00 then 
+
+        # connect internet; grab page
+        
+# read text file
+with open ('test.txt') as f:
+    for line in f:
+        print(line, end = ' ')
+
+        # collect begin end (and title) of tournaments; try search Jan, Feb, Mar..
+
+        # if current.date >= as begin and <= end then activate pylon
+        
+        # else if pylon is on then deactivate pylon
+
