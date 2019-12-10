7e81d78814cdd5e8ac8b25a1ca01199d0bfd703d
MateMatiker <fabian.utesch@scienceforfun.org>
2019-12-10 22:17:15 +0100

    better code, finds text now in fast

--- pylon/starcraft_watch.py
+++ pylon/starcraft_watch.py
@@ -10,9 +10,27 @@
         # connect internet; grab page
         
 # read text file
-with open ('test.txt') as f:
-    for line in f:
-        print(line, end = ' ')
+
+input = open ('liquipedia.net_starcraft2_Premier_Tournaments.html')
+
+text = input.read()
+sub = "2020-03-01"
+
+print(text.find(sub))
+
+for i in range (text.find(sub), (text.find(sub)+10)):
+    print(text[i])
+    
+# TODO
+
+# bestimme aktuelles Jahr
+
+# suche alle start und enddaten für aktuelles Jahr
+
+#with open ('test.txt') as f:
+#    for line in f:
+#        if line == " <td>$300,000":
+#            print(line)
 
         # collect begin end (and title) of tournaments; try search Jan, Feb, Mar..
 
