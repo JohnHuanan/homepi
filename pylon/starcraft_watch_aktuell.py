5421c4b98a3668df6cb84ecfde1e0a1b52ea8e3c
Huanan <john.balzary@web.de>
2020-01-21 22:09:33 +0100

    added pylon start code and crontab

--- pylon/starcraft_watch_aktuell.py
+++ pylon/starcraft_watch_aktuell.py
@@ -1,18 +1,19 @@
 #-*- coding:utf-8 -*-
 import time                                                                             # Das time Modul benötigen wir um das aktuelle Datum bekommen zu können.
 import os                                                                               # os benötigen wir nur dafür, damit wir am Ende einen Tasten-Druck abwarten können bevor sich das Programm beendet.
+
                                                                                         # Die folgende Funktion vergleicht das aktuelle Datum mit den Daten aus den Strings startString und endString... Dazu müssen der Funktion zwei Argumente übergeben werden in String-Form, die das Datum in der Form: JJJJ-MM-TT enthalten
 def dateCompare(startString, endString):
     locTime = time.localtime()                                                          # Das aktuelle Datum wird durch die Funktion time.localtime() ermittelt und in der Variablen locTime gespeichert.
-    if locTime[0] == int(startString[0:4]) and locTime[0] <= int(endString[0:4]):       # locTime[0] beinhaltet das Jahr als integer. Der String wird nur an den Stellen 0 bis 4 eingelesen, denn dort steht das Jahr im Format JJJ-MM-TT. Wenn das aktuelle Jahr mit dem Jahr des übergebenen Strings übereinstimmt und kleiner/gleich dem EndJahr ist, ist die Bedingung erfüllt.
+    if locTime[0] >= int(startString[0:4]) and locTime[0] <= int(endString[0:4]):       # locTime[0] beinhaltet das Jahr als integer. Der String wird nur an den Stellen 0 bis 4 eingelesen, denn dort steht das Jahr im Format JJJ-MM-TT. Wenn das aktuelle Jahr mit dem Jahr des übergebenen Strings übereinstimmt und kleiner/gleich dem EndJahr ist, ist die Bedingung erfüllt.
         yearTrue = True                                                                 # die Variable yearTrue wird erzeugt, die am Ende der Funktion angibt, ob das aktuelle Jahr mit den Jahren in startString und endString stehen übereinstimmt. Wenn es passt True, sonst False
     else:
         yearTrue = False
-    if locTime[1] == int(startString[5:7]) and locTime[1] <= int(endString[5:7]):       # alles wie unter Jahr. Jetzt nur mit Monat. Der aktuelle Monat ist in locTime[1] gespeichert. Im String an den Positionen 5:7. die Variable monthTrue wird auf True gesetzt       
+    if locTime[1] >= int(startString[5:7]) and locTime[1] <= int(endString[5:7]):       # alles wie unter Jahr. Jetzt nur mit Monat. Der aktuelle Monat ist in locTime[1] gespeichert. Im String an den Positionen 5:7. die Variable monthTrue wird auf True gesetzt       
         monthTrue = True
     else:
         monthTrue = False
-    if locTime[2] == int(startString[8:10]) and locTime[1] <= int(endString[8:10]):
+    if locTime[2] >= int(startString[8:10]) and locTime[1] <= int(endString[8:10]):
         dayTrue = True
     else:
         dayTrue = False
@@ -38,15 +39,23 @@ def datenExtrahieren(datei):                                                    
             endDatum.append(text[pos:(pos+10)])
         text = text[(pos+10):]
 
+syscode = 11000
+geraetecode = 3
+
+einschalten = "sudo /home/pi/rcswitch-pi/./send " + str(syscode) + " "+ str(geraetecode) + " " +"1"
+ausschalten = "sudo /home/pi/rcswitch-pi/./send " + str(syscode) + " "+ str(geraetecode) + " " +"0"
+
+os.system(ausschalten)
 
 startDatum = []                                                                         # Wir definieren einen Array startDatum... In diesem sollen die Beginn-Daten aus der Datei eingetragen werden. In String-Form
 endDatum = []                                                                           # Wir definieren einen Array endDatum...   In diesem sollen die End-Daten als Strings gespeichert werden.
 
-datenExtrahieren('liquipedia.net_starcraft2_Premier_Tournaments.html')   
+datenExtrahieren('star.html')   
 
-for i in range (0, (len(startDatum)-1)):
+for i in range (0, (len(startDatum))):
     datumPasst = dateCompare(startDatum[i],endDatum[i])
     if datumPasst == True:
+        os.system(einschalten)
         print("Lieber Fabian! Heute ist ein Starcraft-Turnier!")
         break
     else:
